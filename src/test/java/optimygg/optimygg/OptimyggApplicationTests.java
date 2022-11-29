2e002c575f0e918cc5a9f44889bdf17d44d89b05
ssongjay <jay5326@naver.com>
2022-11-29 20:13:02 +0900

    Initial commit

---
+++ src/test/java/optimygg/optimygg/OptimyggApplicationTests.java
@@ -0,0 +1,13 @@
+package optimygg.optimygg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimyggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
